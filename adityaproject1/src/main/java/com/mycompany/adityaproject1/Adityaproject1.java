a48cf3eb7ef44f493e54841825fef26ea82eaaf6
AdityasPrabowo <stayhalalbroh@gmail.com>
2025-09-01 11:41:25 +0700

    Add files via upload

---
+++ adityaproject1/src/main/java/com/mycompany/adityaproject1/Adityaproject1.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.adityaproject1;
+
+/**
+ *
+ * @author axioo
+ */
+public class Adityaproject1 {
+
+    public static void main(String[] args) {
+        System.out.println("Nama     : Aditya Setyo Prabowo"
+                + "\nNPM      : 2420506037"
+                + "\nAlamat   : Temanggung"
+                + "\nNo_Hp    : 0882007389997");
+    }
+}
