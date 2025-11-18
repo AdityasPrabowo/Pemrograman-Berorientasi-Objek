8d0dd8c6e7afb1bfa766b8423f3847b679be6e4d
AdityasPrabowo <stayhalalbroh@gmail.com>
2025-11-18 13:29:52 +0700

    Add files via upload

---
+++ praktikum11/Perpustakaan.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package praktikum11;
+
+/**
+ *
+ * @author acer
+ */
+class Buku {
+    private String judul;
+ 
+    public Buku(String judul) {
+        this.judul = judul;
+    }
+    public void infoBuku() {
+        System.out.println("Judul Buku: " + judul);
+    }
+}
+class Perpustakaan {
+    private List<Buku> bukuList;
+ 
+    public Perpustakaan() {
+        this.bukuList = new ArrayList<>();
+    }
+       
+    public void tambahBuku(Buku buku) {
+        bukuList.add(buku);
+    }
+ 
+    public void infoPerpustakaan() {
+        for (Buku buku : bukuList) {
+            buku.infoBuku();
+        }
+    }
+}
